513721c5f2db0cc29bcecff79696b9aef5b0c2d1
Oleg667 <oleg-g@yandex.ru>
2022-06-02 19:40:41 +0300

    Initial commit

--- tests/first_test.py
+++ tests/first_test.py
@@ -4,13 +4,8 @@ from app.calculator import Calculator
 
 class TestCalc:
     def setup(self):
-        self.calc=Calculator
-    class TestCalc: # названиекласса тестов
-        def setup(self): # подключаем тестируемый объект Калькулятор
-            self.calc=Calculator
+        self.calc = Calculator
 
-        def test_multiply_calculate_correctly(self): #тест проверки умножения
-            assert self.calc.multiply(self, 2, 2)==4
+    def test_multiply_calculate_correctly(self): #тест проверки умножения
+        assert self.calc.multiply(self, 2, 2)==4
 
-        def test_multiply_calculate_failed(self): #тест проверки умножения
-            assert self.calc.division(self, 6, 2)==3
